45413edaad23af6d6535bfedf20eb9b7bdcce61c
whale <3121279905@qq.com>
2024-01-13 09:02:34 +0800

    getters完成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { ref, computed } from 'vue';
-import { useStore, mapState } from 'vuex'
+import { ref, toRefs, computed } from 'vue';
+import { useStore, mapState, mapGetters } from 'vuex'
 import MapSetUp from './vuex/map函数在setup函数里.js'
 const title = ref("APP")
 
 const store = useStore()
 
-// 解构赋值
-const { name: sname } = store.state
+// 解构赋值,套用toRefs具有响应式
+const { name: sname } = toRefs(store.state)
 
 // mapState(['counter'])返回的是一个对象，里面是函数
 const { counter, name } = MapSetUp(mapState(['counter', 'name']))
@@ -17,17 +17,25 @@ const { counter, name } = MapSetUp(mapState(['counter', 'name']))
 <script>
 export default {
   computed: {
-    ...mapState(['counter'])
+    ...mapState(['level']),
+    ...mapGetters(['fullName'])
   }
 }
 </script>
 <template>
   <h2>{{ title }}</h2>
 
-  <!-- 模板里引用store -->
-  <h3>{{ $store.state.counter }}--{{ sname }}</h3>
+  <!-- 模板里引用store ,直接引用-->
+  <h3>{{ $store.state.counter }}--{{ $store.state.name }}--{{ $store.getters.double }}--{{
+    $store.getters.fullName }}</h3>
 
-  <h3>{{ counter() }}--{{ name() }}</h3>
+
+  <!-- 通过在script里处理后，进行引用 -->
+  <!-- 1.在选项式API中 -->
+  <h3>{{ level }} --{{ fullName }}</h3>
+
+  <!-- 2.在compositionAPI中 -->
+  <h3>{{ counter() }}--{{ name() }}--{{ sname }}</h3>
 </template>
 
 
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -3,7 +3,8 @@ import { createStore } from 'vuex'
 const store = createStore({
     state: () => ({
         counter: 10,
-        name: "STATE"
+        name: "STATE",
+        level: 1000
     }),
     //相当于methodds，是同步函数，方便监听数据变化
     mutations: {
@@ -11,6 +12,12 @@ const store = createStore({
     },
     //相当于computed
     getters: {
+        double(state) {
+            return state.counter * 2
+        },
+        fullName(state) {
+            return state.name + "???"
+        }
 
     },
     //应用时，必须要依赖mutations
